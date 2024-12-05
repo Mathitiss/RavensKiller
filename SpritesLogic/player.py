7d68fbc24cf269afed76b15fb916f5bb7183fc67
Mathitiss <pc.player@mail.ru>
2024-12-05 14:13:11 +0300

    Grenade animation

--- SpritesLogic/player.py
+++ SpritesLogic/player.py
@@ -21,15 +21,20 @@ class Player(pygame.sprite.Sprite):
         # Player Attributes
         self.player_index = 0
         self.player_shoot_index = 0
+        self.grenade_throw_index = 0
         self.gravity = 0
         self.player_speed = 3
         self.shooting_speed = 0.5
+        self.grenade_throw_speed = 0.15
         self.player_damage = 50
         self.player_current_direction = 0  # 0 = right, 1 = left
         self.player_run_right = None
         self.player_run_left = None
         self.player_shoot_right = None
         self.player_shoot_left = None
+        self.grenade_throw_right = None
+        self.grenade_throw_left = None
+        self.throwing = False
 
         # Sprites
         self.player_load_sprites()
@@ -116,8 +121,9 @@ class Player(pygame.sprite.Sprite):
 
     def player_running_animation(self):
         keys = pygame.key.get_pressed()
-        if not keys[pygame.K_SPACE]:    #  and not keys[pygame.K_G]
+        if not keys[pygame.K_SPACE]:
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.throwing = False
                 self.player_index += 0.2
                 self.player_current_direction = 0
                 if self.player_index >= len(self.player_run_right):
@@ -126,6 +132,7 @@ class Player(pygame.sprite.Sprite):
                     self.rect.right += self.player_speed
                 self.image = self.player_run_right[int(self.player_index)]
             elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.throwing = False
                 self.player_index += 0.2
                 self.player_current_direction = 1
                 if self.player_index >= len(self.player_run_left):
@@ -142,17 +149,21 @@ class Player(pygame.sprite.Sprite):
 
     def player_jump(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] or keys[pygame.K_w] and self.rect.bottom == self.PLAYER_GROUND_POS:
-            self.gravity = -20
+        if not keys[pygame.K_g]:
+            if keys[pygame.K_UP] or keys[pygame.K_w] and self.rect.bottom == self.PLAYER_GROUND_POS:
+                self.gravity = -20
+                self.throwing = False
 
     def player_shoot(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.player_current_direction == 0:
+            self.throwing = False
             self.player_shoot_index += self.shooting_speed
             if self.player_shoot_index >= len(self.player_shoot_right):
                 self.player_shoot_index = len(self.player_shoot_right) - 1
             self.image = self.player_shoot_right[int(self.player_shoot_index)]
         elif keys[pygame.K_SPACE] and self.player_current_direction == 1:
+            self.throwing = False
             self.player_shoot_index += self.shooting_speed
             if self.player_shoot_index >= len(self.player_shoot_left):
                 self.player_shoot_index = len(self.player_shoot_right) - 1
@@ -161,7 +172,24 @@ class Player(pygame.sprite.Sprite):
             self.player_shoot_index = 0
 
     def grenade_throw(self):
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_g] and not self.throwing:
+            self.throwing = True
+            self.grenade_throw_index = 0
+        if self.throwing:
+            self.grenade_throw_index += self.grenade_throw_speed
+            if self.player_current_direction == 0:
+                if self.grenade_throw_index >= len(self.grenade_throw_right):
+                    self.grenade_throw_index = len(self.grenade_throw_right) - 1
+                self.image = self.grenade_throw_right[int(self.grenade_throw_index)]
+            elif self.player_current_direction == 1:
+                if self.grenade_throw_index >= len(self.grenade_throw_left):
+                    self.grenade_throw_index = len(self.grenade_throw_left) - 1
+                self.image = self.grenade_throw_left[int(self.grenade_throw_index)]
+            if self.grenade_throw_index >= (len(self.grenade_throw_right) if self.player_current_direction == 0 else len(self.grenade_throw_left)) - 1:
+                self.throwing = False
+        else:
+            self.grenade_throw_index = 0
 
     def player_gravity(self):
         self.gravity += 1
@@ -174,3 +202,4 @@ class Player(pygame.sprite.Sprite):
         self.player_jump()
         self.player_gravity()
         self.player_shoot()
+        self.grenade_throw()
